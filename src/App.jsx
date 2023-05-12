207ffed07730b0fbd2f8bdd5ae3561e1ed1cf7b8
Jury <jury.romblondev@gmail.com>
2023-05-12 23:43:28 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className="content">
+          <h1>Welcome First React APP</h1>
+          <h2>Jury Gregorio Jr</h2>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
